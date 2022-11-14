b27bc4d6d7db621cb86676edac9335302e9b89f1
NotImplementedLife <not.implemented.life@gmail.com>
2022-11-14 09:12:12 +0200

    trying Makefile assets building

--- TwoBitmaps/source/main.cpp
+++ TwoBitmaps/source/main.cpp
@@ -1,6 +1,6 @@
 #include <DSC>
-#include "rainbow8.h"
-#include "face8.h"
+#include "rainbow.h"
+#include "face.h"
 
 using namespace DSC;
 
@@ -11,8 +11,8 @@ class Scene1 : public GenericScene256
 {
 public:
 	void init() override
-	{				
-		GenericScene256::init();				
+	{
+		GenericScene256::init();
 		require_bitmap(MAIN_BG2, &ROA_face8);
 		require_bitmap(MAIN_BG3, &ROA_rainbow8);
 	}	
@@ -23,7 +23,7 @@ public:
 	{		
 		// scroll to highlight the fact there are different backgrounds
 		bgSetScroll(2, face_x(t), face_y(t));
-		bgUpdate();		
+		bgUpdate();
 		t+=0.2;
 	}	
 };
@@ -32,5 +32,5 @@ dsc_launch(Scene1);
 
 #include <math.h>
 
-int face_x(float t) { return 10*sin(t); }
-int face_y(float t) { return 10*sin(t/2); }
+int face_x(float t) { return -72 + 10*sin(t); }
+int face_y(float t) { return -40 + 10*sin(t/2); }
