4485e119910dd1dc5712f54bb19a83b2a8db298e
NotImplementedLife <not.implemented.life@gmail.com>
2022-11-15 14:24:00 +0200

    added sub bitmap bg test

--- TwoBitmaps/source/main.cpp
+++ TwoBitmaps/source/main.cpp
@@ -1,11 +1,13 @@
 #include <DSC>
 #include "rainbow.h"
 #include "face.h"
+#include "sub.h"
 
 using namespace DSC;
 
 int face_x(float t);
 int face_y(float t);
+int buzz(float t);
 
 class Scene1 : public GenericScene256
 {
@@ -15,6 +17,9 @@ public:
 		GenericScene256::init();
 		require_bitmap(MAIN_BG2, &ROA_face8);
 		require_bitmap(MAIN_BG3, &ROA_rainbow8);
+		
+		require_bitmap(SUB_BG2, &ROA_sub8);
+		require_bitmap(SUB_BG3, &ROA_rainbow8);
 	}	
 	
 	float t=0;
@@ -22,9 +27,10 @@ public:
 	void frame() override
 	{		
 		// scroll to highlight the fact there are different backgrounds
-		bgSetScroll(2, face_x(t), face_y(t));
-		bgUpdate();
-		t+=0.2;
+		bgSetScroll(MAIN_BG2, face_x(t), face_y(t));						
+		bgSetScroll(SUB_BG2, buzz(t), 0);
+		bgUpdate();		
+		t+=0.2;				
 	}	
 };
 
@@ -34,3 +40,4 @@ dsc_launch(Scene1);
 
 int face_x(float t) { return -72 + 10*sin(t); }
 int face_y(float t) { return -40 + 10*sin(t/2); }
+int buzz(float t)   { return 3*sin(5*t);}
